3bdbd28bd1cd6063ecbea7c97eb0e500cfa595be
Miroslav Kirnak <kirnak.miroslav@centrum.cz>
2023-02-24 20:23:27 +0100

    requirments.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='app.py',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='spravce',
+    author_email='',
+    description=''
+)
